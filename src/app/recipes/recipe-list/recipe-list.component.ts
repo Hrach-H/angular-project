ac10ba76f60d9d35b891668dd92630cbcb34ad46
hrachya.hovsepyan <hrach.hovsepyan@gmail.com>
2018-08-30 16:58:58 +0400

    Recipes modifications

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'description', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
     new Recipe('Test Recipe', 'description', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
   ];
 
