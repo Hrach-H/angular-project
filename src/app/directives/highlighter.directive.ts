fac97437f5726e06d5e9572ca54d6d910a81288a
hrachya.hovsepyan <hrach.hovsepyan@gmail.com>
2018-09-04 18:18:50 +0400

    Modified appHighlighter directive

--- src/app/directives/highlighter.directive.ts
+++ src/app/directives/highlighter.directive.ts
@@ -1,17 +1,23 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
 })
-export class HighlighterDirective {
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+export class HighlighterDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input('appHighlighter') highlightColor = 'cyan';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor() { }
 
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+  }
+
   @HostListener('mouseenter') onMouseOver() {
-    this.backgroundColor = 'cyan';
+    this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
