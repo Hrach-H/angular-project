87cc1099eeba5e4f19445c083cfc3e5ab936bfbe
hrachya.hovsepyan <hrach.hovsepyan@gmail.com>
2018-09-05 11:48:02 +0400

    appUnless directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { HighlighterDirective } from './directives/highlighter.directive';
+import { UnlessDirective } from './directives/unless.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HighlighterDirective } from './directives/highlighter.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    HighlighterDirective
+    HighlighterDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
